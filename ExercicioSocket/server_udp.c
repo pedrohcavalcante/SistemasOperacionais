99fb47d109eefd589c390d0039f544a2a22ad58a
Pedro Henrique Bezerra Cavalcante <pedrohbcavalcante@A305-27L.imd.local>
2017-09-26 12:21:07 -0300

    server

--- ExercicioSocket/server_udp.c
+++ ExercicioSocket/server_udp.c
@@ -18,9 +18,9 @@ int main(){
   socklen_t slen_s=sizeof(servidor);
   socklen_t slen_c=sizeof(cliente);
   char buf[BUFLEN];
-  if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1){
-	printf("ERRO NA CRICAO DO SOCKET");
-	return 1;
+  if ((s=socket(AF_INET, SOCK_DGRAM, IP PROTO_UDP))==-1){
+    printf("ERRO NA CRICAO DO SOCKET");
+    return 1;
   }
   memset((char*) &servidor, 0, sizeof(servidor));
   servidor.sin_family = AF_INET;
@@ -28,17 +28,17 @@ int main(){
   printf("Setando porta para: %d\n", PORT);
   servidor.sin_addr.s_addr = htonl(INADDR_ANY);
   if(bind(s, (struct sockaddr *) &servidor, sizeof(servidor))==-1){
-	printf("ERRO NO BINDING");
-	return 1;
+    printf("ERRO NO BINDING");
+    return 1;
   }
 
   for(i=0;i<PACOTES;i++){
 
-	if(recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &cliente, &slen_c)==-1){
-		printf("ERRO AO RECEBER DADO");
-		return 1;
-	}
-	printf("Pacote recebido de %s:%d\nDado: %s\n", inet_ntoa(cliente.sin_addr), ntohs(cliente.sin_port), buf);
+    if(recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &cliente, &slen_c)==-1){
+      printf("ERRO AO RECEBER DADO");
+      return 1;
+    }
+    printf("Pacote recebido de %s:%d\nDado: %s\n", inet_ntoa(cliente.sin_addr), ntohs(cliente.sin_port), buf);
   }
 
   sendto(s, "Obrigado!", 10, 0, (struct sockaddr *)&cliente, slen_c);
